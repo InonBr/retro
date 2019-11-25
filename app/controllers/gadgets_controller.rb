5705b676b1cddaf3e15271053fe4ae7f49f5c9fc
Inon Bercu <inonbr@gmail.com>
2019-11-25 16:02:02 +0200

    spaces

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,11 +1,10 @@
 class GadgetsController < ApplicationController
-
   def show
     @gadget = Gadget.find(params[:id])
   end
   skip_before_action :authenticate_user!
+
   def index
     @gadgets = policy_scope(Gadget)
-
   end
 end
